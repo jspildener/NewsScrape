ce636bcdc29ee0c632b3224d57d98ae28769a257
William R Poulson <jessica.spildener@gmail.com>
2017-11-19 21:14:18 -0500

    website scraping functional

--- models/article.js
+++ models/article.js
@@ -3,20 +3,25 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
-	title: {
-		type: String,
-		required: true
-	},
+    title: {
+        type: String,
+        required: true
+    },
 
-	link: {
-		type: String,
-		required: true
-	},
+    link: {
+        type: String,
+        required: true
+    },
 
-	note: {
-		type: Schema.Types.ObjectId,
-		ref: "Note"
-	}
+    paragraph: {
+        type: String,
+        required: true
+    },
+
+    note: {
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }
 });
 
 var Article = mongoose.model("Article", ArticleSchema);
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var axios = require("axios");
 var cheerio = require("cheerio");
 
 var db = require("./models");
+
 var PORT = 3000;
 
 var app = express();
@@ -19,10 +20,47 @@ app.use(express.static("public"));
 
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/mongoScraper", {
-  useMongoClient: true
+    useMongoClient: true
 });
+
+app.get("/scrape", function(req, res) {
+    var results = [];
+    axios.get("http://www.cleveland.com/").then(function(response) {
+        var $ = cheerio.load(response.data);
+        $(".river-item .fullheadline").each(function(i, element) {
+            var result = {};
+            result.link = $(this).children("a").attr("href");
+            result.title = $(this).children("a").text();
+            results.push(result);
+        });
+
+        for (var j = 0; j < results.length; j++) {
+            var article = {
+                "link": results[j].link,
+                "title": results[j].title
+            };
+            saveArticle(article);
+        }
+        res.send("Scrape Complete");
+    });
+});
+
+function saveArticle(article) {
+    axios.get(article.link).then(function(response) {
+        var $ = cheerio.load(response.data);
+        $("article #entryContent").each(function(i, element) {
+            article.paragraph = $(this).children("p:first-child").text() || "Summary not available for this article.";
+            db.Article
+                .create(article).then(function(dbArticle) {})
+                .catch(function(err) {
+                    console.log(article);
+                    res.json(err);
+                });
+        });
+    });
+}
 
 // Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+    console.log("App running on port " + PORT + "!");
 });
