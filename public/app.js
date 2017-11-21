9b4cb0806b8690661fa3aea30d44b2acbb8aa530
William R Poulson <jessica.spildener@gmail.com>
2017-11-21 16:02:51 -0500

    delete article working

--- public/app.js
+++ public/app.js
@@ -23,7 +23,6 @@ $("body").on("submit", ".save-form", function(e) {
 });
 
 $("#saved").on("click", function() {
-    console.log("hi");
     $.getJSON("/savedarticles", function(data) {
         $("#articles").empty();
         for (var i = 0; i < data.length; i++) {
@@ -32,8 +31,27 @@ $("#saved").on("click", function() {
                 "<h2 data-id='" + data[i]._id + "'>" +
                 "<a href=" + data[i].link + ">" + data[i].title + "</a>" +
                 "</h2>" + "</div>" + "<div class='col-md-3'>" +
-                "<form class='save-form' data-id='" + data[i]._id + "'><button class='btn btn-primary' type='submit'>Delete</button></form>" +
+                "<form class='delete-form' data-id='" + data[i]._id + "'><button class='btn btn-danger' type='submit'>Delete</button></form>" +
+                //"<form class='save-form' data-id='" + data[i]._id + "'><button class='btn btn-primary' class='addnotebutton' type='submit'>Add Note</button></form>" +
                 "</div>" + "</div>" + "<p>" + data[i].paragraph + "</p>" + "</div>");
         }
     });
 });
+
+$("body").on("submit", ".delete-form", function(e) {
+    console.log("deleted");
+    e.preventDefault();
+    $.ajax({
+        url: '/deletearticle/' + $(this).data("id"),
+        type: 'post'
+    });
+});
+// $("body").on("click", ".addnotebutton", function() {
+//     console.log("click");
+//     var thisId = $(this).attr("data-id");
+//     console.log(this);
+//     $("#notes").append(
+//         "<div>" + "<div class='row'>" + "<div class='col-md-9'>" +
+//         "<form ><input type='text'><button class='btn btn-primary' type='submit'>Add Note</button></form>" +
+//         +"</div>");
+// });
